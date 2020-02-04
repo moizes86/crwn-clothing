46015b423bc6d30f5d527866706789da569a4fab
[Moshe Mantsur] <[moshe.mn86@gmail.com]>
2020-02-04 20:13:19 +0200

    basic routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React from "react";
 import Homepage from "./pages/homepage/homepage.component";
-import './App.css';
+import { Switch, Route } from "react-router-dom";
+import "./App.css";
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
 
 function App() {
   return (
-    <div >
-      <Homepage/>
+    <div>
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
