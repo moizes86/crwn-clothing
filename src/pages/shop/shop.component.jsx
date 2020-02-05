96dec4132fbb279385d3e12f03e6c426edd2c7e0
[Moshe Mantsur] <[moshe.mn86@gmail.com]>
2020-02-05 13:48:14 +0200

    built a shop page, collection-preview component and collection-item component

---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Shop_Data from "./shop.data";
+import CollectionPreview from "../preview-collection/preview-collection.component";
+
+class ShopPage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      collections: Shop_Data
+    };
+  }
+
+  render() {
+      const {collections} = this.state;
+    return (
+      <div className="shop-page">
+        {
+            collections.map(({id, ...otherCollectionProps})=>(
+                <CollectionPreview key={id} {...otherCollectionProps} />
+            ))
+        }
+      </div>
+    );
+  }
+}
+
+export default ShopPage;
