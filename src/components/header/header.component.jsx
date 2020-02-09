b594029d7ff201f7ce520a3fc0329ed1f8d085b5
[Moshe Mantsur] <[moshe.mn86@gmail.com]>
2020-02-09 20:24:33 +0200

    Added redux and implemented userReducer and userAction to Header and App components

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { ReactComponent as Logo } from "../../assets/crown.svg";
- import { auth } from "../../firebase/firebase.utils";
+import { connect } from "react-redux";
 
+import { auth } from "../../firebase/firebase.utils";
+
+import { ReactComponent as Logo } from "../../assets/crown.svg";
 import "./header.styles.scss";
 
-const Header = ({currentUser}) => (
+const Header = ({ currentUser }) => (
   <div className="header">
     <Link className="logo-container" to="/">
       <Logo className="logo" />
@@ -17,13 +19,22 @@ const Header = ({currentUser}) => (
       <Link className="option" to="/shop">
         CONTACT
       </Link>
-        {
-          currentUser?
-          <div className="option" onClick={()=> auth.signOut()}>SIGN OUT</div>:
-          <Link className="option" to="/login">SIGN IN</Link>
-        }
-      </div>
-  </div>
+      {currentUser ? (
+        <div className="option" onClick={() => auth.signOut()}>
+          SIGN OUT
+        </div>
+      ) : (
+        <Link className="option" to="/login">
+          SIGN IN
+        </Link>
+      )}
+    </div>
+    {console.log(currentUser)}
+    </div>
 );
 
-export default Header;
+const mapStateToProps = state => ({
+  currentUser: state.user.currentUser,
+});
+
+export default connect(mapStateToProps)(Header);
