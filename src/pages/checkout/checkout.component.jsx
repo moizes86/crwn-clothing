e2ecb52197634b4c5fc7da7450465db727033f3b
[Moshe Mantsur] <[moshe.mn86@gmail.com]>
2020-02-26 22:32:04 +0200

    updating stripe integration and updated favicon

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -4,6 +4,7 @@ import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
 
 import {
   selectCartItems,
@@ -12,7 +13,7 @@ import {
 
 import "./checkout.styles.scss";
 
-const CheckoutPage = ({ cartItems, totalVaule }) => (
+const CheckoutPage = ({ cartItems, totalValue }) => (
   <div className="checkout-page">
     <div className="checkout-header">
       <div className="header-blocks">
@@ -35,14 +36,24 @@ const CheckoutPage = ({ cartItems, totalVaule }) => (
       <CheckoutItem key={cartItem.id} cartItem={cartItem} />
     ))}
     <div className="total">
-      <span>TOTAL: ${totalVaule}</span>
+      <span>TOTAL: ${totalValue}</span>
+    </div>
+    <StripeCheckoutButton className='button' price={totalValue} />
+    <div className='test-warning'>
+    *Please use the following test credit card for payments*
+    <br/>
+    4242 4242 4242 4242
+    <br/>
+    Date: any 
+    <br/>
+    CVV: any
     </div>
   </div>
 );
 
 const mapStateToProps = createStructuredSelector({
   cartItems: selectCartItems,
-  totalVaule: selectCartTotal
+  totalValue: selectCartTotal
 });
 
 export default connect(mapStateToProps)(CheckoutPage);
